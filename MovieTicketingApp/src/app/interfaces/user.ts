9a6c9895bfdbb1ca56591c8acab7f6438d4ced06
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-08 21:45:10 +0530

    created user types

---
+++ MovieTicketingApp/src/app/interfaces/user.ts
@@ -0,0 +1,22 @@
+export interface UserSignup {
+    id: number,
+    username: string,
+    password: string,
+    email: string
+}
+
+export interface UserLoginRequest {
+    username: string,
+    password: string
+}
+
+export interface UserLoginResponse {
+    refreshToken: String,
+    accessToken: string,
+    user: {
+        id: number,
+        username: string,
+        email: string,
+        role: string
+    }
+}
