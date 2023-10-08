e15817adc056270f7546bf26db472dbc2dca7c2e
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-08 21:46:45 +0530

    updated signup component

--- MovieTicketingApp/src/app/signup/signup.component.ts
+++ MovieTicketingApp/src/app/signup/signup.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+	selector: 'app-signup',
+	templateUrl: './signup.component.html',
+	styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+	signupForm!: FormGroup
+
+	constructor(private fb: FormBuilder) { }
+
+	ngOnInit(): void {
+		this.signupForm = this.fb.group({
+
+			username: new FormControl('', {
+				updateOn: 'blur',
+				validators: [
+					Validators.required,
+					Validators.pattern(/^[A-Za-z][A-Za-z_0-9]{7,30}$/g)
+				]
+			}),
+
+			password: new FormControl('', {
+				updateOn: 'blur',
+				validators: [
+					Validators.required,
+					Validators.minLength(7)
+				]
+			}),
+
+			email: new FormControl('', {
+				validators: [Validators.required, Validators.email]
+			})
+		});
+	}
+
+	signup() {
+		console.log(this.signupForm.getRawValue());
+	}
 }
