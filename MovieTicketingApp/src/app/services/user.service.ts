aca7c588583c39945698bb570851ef1e40d1c19b
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-09 18:46:18 +0530

    created user service

---
+++ MovieTicketingApp/src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserLoginRequest, UserSignup } from '../interfaces/user';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UserService {
+
+	constructor(private http: HttpClient) { }
+
+	signup(user: UserSignup) {
+		return this.http.post("https://localhost:7064/Authentication/register", user);
+	}
+
+	login(user: UserLoginRequest) {
+		return this.http.post("https://localhost:7064/Authentication/login", user);
+	}
+
+	refresh(refreshToken: string) {
+		return this.http.post("https://localhost:7064/Authentication/refresh ", refreshToken);
+	}
+
+	logout(refreshToken: string) {
+		return this.http.post("https://localhost:7064/Authentication/logout", refreshToken);
+	}
+}
