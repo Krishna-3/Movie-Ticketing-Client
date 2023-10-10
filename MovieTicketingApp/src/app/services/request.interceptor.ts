ae9fe161271d628313cae139a4da1e21d8cc7eb8
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-10 21:12:19 +0530

    updated response interceptor

--- MovieTicketingApp/src/app/services/request.interceptor.ts
+++ MovieTicketingApp/src/app/services/request.interceptor.ts
@@ -7,10 +7,9 @@ import {
 	HttpErrorResponse,
 	HttpClient
 } from '@angular/common/http';
-import { Observable, catchError } from 'rxjs';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
 import { LocalStorageService } from './local-storage.service';
 import { JwtService } from './jwt.service';
-import { Router } from '@angular/router';
 
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
@@ -30,25 +29,31 @@ export class RequestInterceptor implements HttpInterceptor {
 		});
 
 		return next.handle(req).pipe(
-			catchError((err: HttpErrorResponse, caught$) => {
+			catchError((error) => {
+				if (token !== null) {
+					if (error.status === 401 && this.jwtService.isTokenExpired(token as string)) {
+						const refresh = this.localStorageService.get('refreshToken');
 
-				if (err.status === 401) {
-					const refresh = this.localStorageService.get('refreshToken');
+						if (refresh !== null) {
+							return this.jwtService.getNewRefresh(refresh).pipe(
+								switchMap(() => {
 
-					if (refresh !== null) {
-						this.jwtService.getNewRefresh(refresh);
-						const token = this.localStorageService.get("AccessToken");
-						const newReq = request.clone({
-							url: request.url,
-							setHeaders: {
-								Authorization: `Bearer ${token}`
-							}
-						});
-						return this.http.request(newReq)
+									const token = this.localStorageService.get("AccessToken");
+									const newReq = request.clone({
+										url: request.url,
+										setHeaders: {
+											Authorization: `Bearer ${token}`
+										}
+									});
+
+									return next.handle(newReq)
+								})
+							)
+						}
 					}
 				}
-				return caught$;
+				return throwError(() => error);
 			})
-		);
+		)
 	}
 }
