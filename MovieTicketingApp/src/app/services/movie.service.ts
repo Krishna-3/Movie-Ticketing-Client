b00c090d828f8cb150602ae174e9b4cf7437c568
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-11 23:02:56 +0530

    created movie service

---
+++ MovieTicketingApp/src/app/services/movie.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MovieService {
+
+    constructor(private http: HttpClient) { }
+
+    getMovies() {
+        return this.http.get("https://localhost:7064/Movie");
+    }
+
+    getLocations() {
+        return this.http.get("https://localhost:7064/Location");
+    }
+
+    setLocation(city: string) {
+        return this.http.post(`https://localhost:7064/State/location?city=${city}`, null);
+    }
+
+    setLanguage(languageCode: string) {
+        return this.http.post(`https://localhost:7064/State/language?languageCode=${languageCode}`, null);
+    }
+}
