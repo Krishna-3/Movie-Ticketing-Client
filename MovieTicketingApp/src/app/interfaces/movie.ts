4a9c4a33fa51d27bf31c53c9fe55fdd2a46639a4
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-11 23:01:51 +0530

    created movie types

---
+++ MovieTicketingApp/src/app/interfaces/movie.ts
@@ -0,0 +1,28 @@
+export interface City {
+    id: number,
+    city: string
+}
+
+export interface MovieEn {
+    id: number,
+    titleEn: string,
+    descriptionEn: string,
+    languageEn: string,
+    rating: number
+}
+
+export interface MovieTe {
+    id: number,
+    titleTe: string,
+    descriptionTe: string,
+    languageTe: string,
+    rating: number
+}
+
+export interface MovieHi {
+    id: number,
+    titleHi: string,
+    descriptionHi: string,
+    languageHi: string,
+    rating: number
+}
