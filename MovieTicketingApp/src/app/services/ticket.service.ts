dd380a5a2b95816ebd4c7cbc47165d067c9c146b
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-13 20:28:47 +0530

    created ticket service

---
+++ MovieTicketingApp/src/app/services/ticket.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Ticket } from '../interfaces/ticket';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class TicketService {
+
+	ticket: Ticket = {
+		SeatId: 0,
+		MovieId: 0,
+		TheatreId: 0,
+		UserId: 0
+	};
+
+	constructor() { }
+}
