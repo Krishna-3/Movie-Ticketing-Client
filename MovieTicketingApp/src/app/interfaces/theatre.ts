9ec690d5e3435eec6713813f5029979e3ff72c70
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-13 20:27:05 +0530

    created theatre type

---
+++ MovieTicketingApp/src/app/interfaces/theatre.ts
@@ -0,0 +1,22 @@
+export interface TheatreEn {
+    id: string,
+    nameEn: string,
+    type: string
+}
+
+export interface TheatreTe {
+    id: string,
+    nameTe: string,
+    type: string
+}
+
+export interface TheatreHi {
+    id: string,
+    nameHi: string,
+    type: string
+}
+
+export interface Theatre {
+    id: string,
+    name: string
+}
