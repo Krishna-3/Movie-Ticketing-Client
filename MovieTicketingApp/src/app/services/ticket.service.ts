cdf8034f54da4ae6070bf5414cb97554a11a1523
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-14 19:53:23 +0530

    added get tickets method

--- MovieTicketingApp/src/app/services/ticket.service.ts
+++ MovieTicketingApp/src/app/services/ticket.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { TicketId } from '../interfaces/ticket';
 import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from './local-storage.service';
+import { JwtService } from './jwt.service';
 
 @Injectable({
 	providedIn: 'root'
@@ -16,11 +17,17 @@ export class TicketService {
 	};
 
 	constructor(private http: HttpClient,
-		private localStorageService: LocalStorageService) { }
+		private localStorageService: LocalStorageService,
+		private jwtService: JwtService) { }
 
 	bookTicket(ticket: TicketId) {
 		const timeId = this.localStorageService.get('timeId');
 		const date = this.localStorageService.get('date');
 		return this.http.post(`https://localhost:7064/Ticket/${timeId}?date=${date}`, ticket);
+	}
+
+	getTickets() {
+		const userId = this.jwtService.getId(this.localStorageService.get('accessToken') as string);
+		return this.http.get(`https://localhost:7064/Ticket/${userId}`);
 	}
 }
