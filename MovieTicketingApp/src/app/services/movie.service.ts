0ef958b284fb75d7f43115d6a2004193507dc6c4
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-14 15:18:26 +0530

    added get seats route

--- MovieTicketingApp/src/app/services/movie.service.ts
+++ MovieTicketingApp/src/app/services/movie.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Ticket } from '../interfaces/ticket';
 
 @Injectable({
     providedIn: 'root'
@@ -26,5 +27,9 @@ export class MovieService {
 
     getTheatres(movieId: number) {
         return this.http.get(`https://localhost:7064/Theatre/movie/${movieId}`);
+    }
+
+    getSeats(ticket: Ticket) {
+        return this.http.post(`https://localhost:7064/Seat/${ticket.timeId}?date=${ticket.date}`, { ticketId: { SeatId: ticket.SeatId, MovieId: ticket.MovieId, TheatreId: ticket.TheatreId, UserId: ticket.UserId } });
     }
 }
