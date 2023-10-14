56a8fc11f97b0b4fb312c32c9a1b2bf352395d23
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-14 19:52:54 +0530

    added ticket parser

--- MovieTicketingApp/src/app/services/parse.service.ts
+++ MovieTicketingApp/src/app/services/parse.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Movie } from '../interfaces/movie';
 import { Theatre } from '../interfaces/theatre';
+import { LocalStorageService } from './local-storage.service';
+import { TicketResponse } from '../interfaces/ticket';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ParseService {
 
-	constructor() { }
+	constructor(private localStorageService: LocalStorageService) { }
 
 	parseMovies(res: Object) {
 		let movieArray: Movie[] = [];
 
 		if (Array.isArray(res)) {
+			this.localStorageService.set('language', res[0].type)
 			movieArray = res.map((movieData) => {
 
 				if (movieData.type === "en") {
@@ -54,6 +57,7 @@ export class ParseService {
 		let theatreArray: Theatre[] = [];
 
 		if (Array.isArray(res)) {
+			this.localStorageService.set('language', res[0].type)
 			theatreArray = res.map((theatreData) => {
 
 				if (theatreData.type === "en") {
@@ -78,5 +82,95 @@ export class ParseService {
 			});
 		}
 		return theatreArray;
+	}
+
+	parseTickets(language: string, res: Object) {
+		let ticketArray: TicketResponse[] = [];
+
+		if (Array.isArray(res)) {
+
+			if (language === 'en') {
+				ticketArray = res.map(t => {
+					return {
+						seat: t.seat,
+						time: t.time,
+						movieTheatre: {
+							id: t.movieTheatre.id,
+							movieId: t.movieTheatre.movieId,
+							theatreId: t.movieTheatre.theatreId,
+							movie: {
+								id: t.movieTheatre.movie.id,
+								title: t.movieTheatre.movie.titleEn,
+								rating: t.movieTheatre.movie.rating,
+								description: t.movieTheatre.movie.descriptionEn,
+								language: t.movieTheatre.movie.languageEn,
+								photo: t.movieTheatre.movie.photo,
+							},
+							theatre: {
+								id: t.movieTheatre.theatre.id,
+								name: t.movieTheatre.theatre.nameEn,
+								location: t.movieTheatre.theatre.location
+							}
+						}
+					}
+				})
+			}
+
+			else if (language === 'hi') {
+				ticketArray = res.map(t => {
+					return {
+						seat: t.seat,
+						time: t.time,
+						movieTheatre: {
+							id: t.movieTheatre.id,
+							movieId: t.movieTheatre.movieId,
+							theatreId: t.movieTheatre.theatreId,
+							movie: {
+								id: t.movieTheatre.movie.id,
+								title: t.movieTheatre.movie.titleHi,
+								rating: t.movieTheatre.movie.rating,
+								description: t.movieTheatre.movie.descriptionHi,
+								language: t.movieTheatre.movie.languageHi,
+								photo: t.movieTheatre.movie.photo,
+							},
+							theatre: {
+								id: t.movieTheatre.theatre.id,
+								name: t.movieTheatre.theatre.nameHi,
+								location: t.movieTheatre.theatre.location
+							}
+						}
+					}
+				})
+			}
+
+			else {
+				ticketArray = res.map(t => {
+					return {
+						seat: t.seat,
+						time: t.time,
+						movieTheatre: {
+							id: t.movieTheatre.id,
+							movieId: t.movieTheatre.movieId,
+							theatreId: t.movieTheatre.theatreId,
+							movie: {
+								id: t.movieTheatre.movie.id,
+								title: t.movieTheatre.movie.titleTe,
+								rating: t.movieTheatre.movie.rating,
+								description: t.movieTheatre.movie.descriptionTe,
+								language: t.movieTheatre.movie.languageTe,
+								photo: t.movieTheatre.movie.photo,
+							},
+							theatre: {
+								id: t.movieTheatre.theatre.id,
+								name: t.movieTheatre.theatre.nameTe,
+								location: t.movieTheatre.theatre.location
+							}
+						}
+					}
+				})
+			}
+		}
+
+		return ticketArray;
 	}
 }
