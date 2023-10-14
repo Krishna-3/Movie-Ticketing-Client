e84d85bbfb25ad122f2632f55bad279c39f04da0
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-14 19:52:34 +0530

    created ticket component

---
+++ MovieTicketingApp/src/app/ticket/ticket.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TicketService } from '../services/ticket.service';
+import { Subscription, switchMap } from 'rxjs';
+import { Movie, Seat } from '../interfaces/movie';
+import { Theatre } from '../interfaces/theatre';
+import { TicketResponse } from '../interfaces/ticket';
+import { MovieService } from '../services/movie.service';
+import { ParseService } from '../services/parse.service';
+import { LocalStorageService } from '../services/local-storage.service';
+
+@Component({
+	selector: 'app-ticket',
+	templateUrl: './ticket.component.html',
+	styleUrls: ['./ticket.component.css']
+})
+export class TicketComponent implements OnInit, OnDestroy {
+
+	subscription1!: Subscription;
+
+	subscription2!: Subscription;
+
+	tickets!: TicketResponse[];
+
+	constructor(private ticketService: TicketService,
+		private localStorageService: LocalStorageService,
+		private parseService: ParseService) { }
+
+	ngOnInit(): void {
+		this.subscription1 = this.ticketService.getTickets().subscribe({
+			next: data => this.tickets = this.parseService.parseTickets(this.localStorageService.get('language') as string, data),
+			error: err => console.log(err)
+		})
+
+	}
+	ngOnDestroy(): void {
+		if (this.subscription1) {
+			this.subscription1.unsubscribe();
+		}
+		if (this.subscription2) {
+			this.subscription2.unsubscribe();
+		}
+	}
+}
