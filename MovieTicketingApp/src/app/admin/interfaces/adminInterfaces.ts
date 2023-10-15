7803321621c2b1df11a89b412228f4cfdefc378b
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-15 19:18:34 +0530

    created admin interface

---
+++ MovieTicketingApp/src/app/admin/interfaces/adminInterfaces.ts
@@ -0,0 +1,18 @@
+export interface City {
+    id: number,
+    city: string
+}
+
+export interface TheatreModel {
+    id: number,
+    nameEn: string,
+    nameTe: string,
+    nameHi: string,
+    locationId: number
+}
+
+export interface TheatreName {
+    nameEn: string,
+    nameTe: string,
+    nameHi: string
+}
