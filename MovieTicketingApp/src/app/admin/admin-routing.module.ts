a6dd2674db5f1d9929394f0855bd3fd0a9f04ece
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-15 17:29:11 +0530

    added routes

--- MovieTicketingApp/src/app/admin/admin-routing.module.ts
+++ MovieTicketingApp/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationComponent } from './location/location.component';
+import { HomeComponent } from '../home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'location', component: LocationComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
