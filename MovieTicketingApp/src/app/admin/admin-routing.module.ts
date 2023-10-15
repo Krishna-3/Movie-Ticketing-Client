afa19a67c5aec6605da05c4b39ac0a6513124eb9
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-15 19:19:51 +0530

    added theatre path

--- MovieTicketingApp/src/app/admin/admin-routing.module.ts
+++ MovieTicketingApp/src/app/admin/admin-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LocationComponent } from './location/location.component';
-import { HomeComponent } from '../home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { TheatreComponent } from './theatre/theatre.component';
 
 const routes: Routes = [
   { path: 'location', component: LocationComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'theatre', component: TheatreComponent },
   { path: '', component: AdminComponent },
 ];
 
