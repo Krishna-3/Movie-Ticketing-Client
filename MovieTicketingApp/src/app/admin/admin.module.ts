0ea4faa2833f622ca1ca4929406fb1aa6619e0e0
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-15 17:29:01 +0530

    added declarations and imports

--- MovieTicketingApp/src/app/admin/admin.module.ts
+++ MovieTicketingApp/src/app/admin/admin.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { LocationComponent } from './location/location.component';
+import { AdminComponent } from './admin/admin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LocationComponent,
+    AdminComponent
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class AdminModule { }
