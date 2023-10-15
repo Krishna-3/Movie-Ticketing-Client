8e5b7dd2003cf7909aeca0e3b5e12b9d69f20d93
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-15 23:02:52 +0530

    created movie-location component

---
+++ MovieTicketingApp/src/app/admin/movie-location/movie-location.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import { AdminMovieLocationService } from '../services/admin-movie-location.service';
+import { AdminLocationService } from '../services/admin-location.service';
+import { Router } from '@angular/router';
+import { AdminMovieService } from '../services/admin-movie.service';
+import { City, MovieLocationId, MovieLocationModel, MovieModel } from '../interfaces/adminInterfaces';
+
+@Component({
+	selector: 'app-movie-location',
+	templateUrl: './movie-location.component.html',
+	styleUrls: ['./movie-location.component.css']
+})
+export class MovieLocationComponent implements OnInit, OnDestroy {
+
+	subscription1!: Subscription;
+
+	subscription2!: Subscription;
+
+	movieLocationForm!: FormGroup;
+
+	movies$!: Observable<MovieModel[]>;
+
+	cities$!: Observable<City[]>;
+
+	movieLocations$!: Observable<MovieLocationModel[]>
+
+	constructor(private fb: FormBuilder,
+		private adminMovieLocationService: AdminMovieLocationService,
+		private adminLocationService: AdminLocationService,
+		private adminMovieService: AdminMovieService,
+		private router: Router) { }
+
+	ngOnInit(): void {
+		this.movies$ = this.adminMovieService.getMovies() as Observable<MovieModel[]>;
+		this.cities$ = this.adminLocationService.getLocations() as Observable<City[]>;
+		this.movieLocations$ = this.adminMovieLocationService.getMovieLocations() as Observable<MovieLocationModel[]>;
+
+		this.movieLocationForm = this.fb.group({
+			movieId: new FormControl('', { validators: [Validators.required] }),
+			locationId: new FormControl('', { validators: [Validators.required] }),
+		})
+	}
+
+	createMovieLocation() {
+		const movieLocation: MovieLocationId = {
+			locationId: this.movieLocationForm.get('locationId')?.value,
+			movieId: this.movieLocationForm.get('movieId')?.value
+		}
+
+		this.subscription1 = this.adminMovieLocationService.createMovieLocation(movieLocation).subscribe();
+	}
+
+	deleteMovieLocation(mlId: number) {
+		this.subscription2 = this.adminMovieLocationService.deleteMovieLocation(mlId).subscribe();
+	}
+
+	ngOnDestroy(): void {
+		if (this.subscription1) {
+			this.subscription1.unsubscribe();
+		}
+		if (this.subscription2) {
+			this.subscription2.unsubscribe();
+		}
+	}
+}
