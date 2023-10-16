26d42f9e9a1dffe819c91de8c06af68006963e23
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-16 08:55:40 +0530

    added movie-theatre route

--- MovieTicketingApp/src/app/admin/admin-routing.module.ts
+++ MovieTicketingApp/src/app/admin/admin-routing.module.ts
@@ -5,18 +5,20 @@ import { AdminComponent } from './admin/admin.component';
 import { TheatreComponent } from './theatre/theatre.component';
 import { MovieComponent } from './movie/movie.component';
 import { MovieLocationComponent } from './movie-location/movie-location.component';
+import { MovieTheatreComponent } from './movie-theatre/movie-theatre.component';
 
 const routes: Routes = [
-  { path: 'location', component: LocationComponent },
-  { path: 'admin', component: AdminComponent },
-  { path: 'theatre', component: TheatreComponent },
-  { path: 'movie', component: MovieComponent },
-  { path: 'movie-location', component: MovieLocationComponent },
-  { path: '', component: AdminComponent },
+	{ path: 'location', component: LocationComponent },
+	{ path: 'admin', component: AdminComponent },
+	{ path: 'theatre', component: TheatreComponent },
+	{ path: 'movie', component: MovieComponent },
+	{ path: 'movie-location', component: MovieLocationComponent },
+	{ path: 'movie-theatre', component: MovieTheatreComponent },
+	{ path: '', component: AdminComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class AdminRoutingModule { }
