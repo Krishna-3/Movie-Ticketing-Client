a782e7cfc837dfba98bbde3fa2879b4aa45d1521
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-17 18:13:08 +0530

    added logout

--- MovieTicketingApp/src/app/app.component.ts
+++ MovieTicketingApp/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Subscription } from 'rxjs';
 import { MovieService } from './services/movie.service';
 import { Router } from '@angular/router';
 import { LocalStorageService } from './services/local-storage.service';
+import { UserService } from './services/user.service';
+import { JwtService } from './services/jwt.service';
 
 @Component({
 	selector: 'app-root',
@@ -12,14 +14,18 @@ import { LocalStorageService } from './services/local-storage.service';
 })
 export class AppComponent implements OnInit {
 
-	subscription!: Subscription;
+	subscription1!: Subscription;
+
+	subscription2!: Subscription;
 
 	languageForm!: FormGroup
 
 	constructor(private fb: FormBuilder,
 		private movieService: MovieService,
 		private router: Router,
-		private localStorageService: LocalStorageService) { }
+		private localStorageService: LocalStorageService,
+		private userService: UserService,
+		private jwtService: JwtService) { }
 
 	ngOnInit(): void {
 		this.languageForm = this.fb.group({
@@ -28,16 +34,33 @@ export class AppComponent implements OnInit {
 	}
 
 	setLanguage() {
-		this.subscription = this.movieService.setLanguage(this.languageForm.get('preferredLanguage')?.value).subscribe(data => {
+		this.subscription1 = this.movieService.setLanguage(this.languageForm.get('preferredLanguage')?.value).subscribe(data => {
 			const present = this.router.url;
 			this.router.navigate(['/signup']).then(s => this.router.navigateByUrl(present));
 			this.localStorageService.set('language', this.languageForm.get('preferredLanguage')?.value)
 		});
 	}
 
+	logout() {
+		const userId: string = this.jwtService.getId(this.localStorageService.get('accessToken') as string) as string;
+
+		if (userId.length === 0)
+			return
+
+		this.subscription2 = this.userService.logout(userId).subscribe(data => this.router.navigate(['/login']));
+		this.localStorageService.remove('accessToken');
+		this.localStorageService.remove('refreshToken');
+		this.localStorageService.remove('timeId');
+		this.localStorageService.remove('date');
+		this.localStorageService.remove('language');
+	}
+
 	ngOnDestroy(): void {
-		if (this.subscription) {
-			this.subscription.unsubscribe();
+		if (this.subscription1) {
+			this.subscription1.unsubscribe();
+		}
+		if (this.subscription2) {
+			this.subscription2.unsubscribe();
 		}
 	}
 }
