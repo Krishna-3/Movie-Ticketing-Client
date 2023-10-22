ae0cc8f0575fb6691701fa34518836ab9be77ba8
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-10-22 18:55:47 +0530

    updated routing

--- MovieTicketingApp/src/app/admin/admin-routing.module.ts
+++ MovieTicketingApp/src/app/admin/admin-routing.module.ts
@@ -6,15 +6,17 @@ import { TheatreComponent } from './theatre/theatre.component';
 import { MovieComponent } from './movie/movie.component';
 import { MovieLocationComponent } from './movie-location/movie-location.component';
 import { MovieTheatreComponent } from './movie-theatre/movie-theatre.component';
+import { adminGuard } from '../guards/admin.guard';
+import { userGuard } from '../guards/user.guard';
 
 const routes: Routes = [
-	{ path: 'location', component: LocationComponent },
-	{ path: 'admin', component: AdminComponent },
-	{ path: 'theatre', component: TheatreComponent },
-	{ path: 'movie', component: MovieComponent },
-	{ path: 'movie-location', component: MovieLocationComponent },
-	{ path: 'movie-theatre', component: MovieTheatreComponent },
-	{ path: '', component: AdminComponent },
+	{ path: 'location', component: LocationComponent, canActivate: [userGuard, adminGuard] },
+	{ path: 'admin', component: AdminComponent, canActivate: [userGuard, adminGuard] },
+	{ path: 'theatre', component: TheatreComponent, canActivate: [userGuard, adminGuard] },
+	{ path: 'movie', component: MovieComponent, canActivate: [userGuard, adminGuard] },
+	{ path: 'movie-location', component: MovieLocationComponent, canActivate: [userGuard, adminGuard] },
+	{ path: 'movie-theatre', component: MovieTheatreComponent, canActivate: [userGuard, adminGuard] },
+	{ path: '', component: AdminComponent, canActivate: [userGuard, adminGuard] },
 ];
 
 @NgModule({
--- MovieTicketingApp/src/app/app-routing.module.ts
+++ MovieTicketingApp/src/app/app-routing.module.ts
@@ -8,16 +8,18 @@ import { TheatresComponent } from './theatres/theatres.component';
 import { TheatreComponent } from './theatre/theatre.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { ProfileComponent } from './profile/profile.component';
+import { userGuard } from './guards/user.guard';
+import { adminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
-	{ path: 'home', component: HomeComponent },
+	{ path: 'home', component: HomeComponent, canActivate: [userGuard] },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'signup', component: SignupComponent },
-	{ path: 'movie/:movieId', component: TheatresComponent },
-	{ path: 'movie/:movieId/theatre/:theatreId', component: TheatreComponent },
-	{ path: 'ticket', component: TicketComponent },
-	{ path: 'profile', component: ProfileComponent },
-	{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(e => e.AdminModule) },
+	{ path: 'movie/:movieId', component: TheatresComponent, canActivate: [userGuard] },
+	{ path: 'movie/:movieId/theatre/:theatreId', component: TheatreComponent, canActivate: [userGuard] },
+	{ path: 'ticket', component: TicketComponent, canActivate: [userGuard] },
+	{ path: 'profile', component: ProfileComponent, canActivate: [userGuard] },
+	{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(e => e.AdminModule), canActivate: [userGuard, adminGuard], canMatch: [adminGuard] },
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: '**', component: NotFoundComponent },
 ];
